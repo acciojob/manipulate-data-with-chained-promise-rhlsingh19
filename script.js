244cd6443fd399b22f46fd88d291d3702dbf268a
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2024-02-22 21:56:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,26 @@
-//your JS code here. If required.
-// Function to return a promise that resolves with an array of numbers after 3 seconds
-function getNumbers() {
-    return new Promise(resolve => {
+function manipulateData(inputArray) {
+      return new Promise((resolve) => {
         setTimeout(() => {
-            resolve([1, 2, 3, 4]);
+          resolve(inputArray);
         }, 3000);
-    });
-}
+      }).then((array) => {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            const evens = array.filter(num => num % 2 === 0);
+            document.getElementById('output').innerText = evens.join(', ');
+            resolve(evens);
+          }, 1000);
+        });
+      }).then((evens) => {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            const doubled = evens.map(num => num * 2);
+            document.getElementById('output').innerText = doubled.join(', ');
+            resolve(doubled);
+          }, 2000);
+        });
+      });
+    }
 
-// Function to filter out odd numbers
-function filterOddNumbers(numbers) {
-    return numbers.filter(num => num % 2 === 0);
-}
-
-// Function to multiply even numbers by 2
-function multiplyEvenNumbers(numbers) {
-    return numbers.map(num => num % 2 === 0 ? num * 2 : num);
-}
-
-// Function to update the text of an HTML element with the ID "output"
-function updateOutput(text) {
-    document.getElementById('output').innerText = text;
-}
-
-// Chain promises to manipulate the data and update the output
-getNumbers()
-    .then(numbers => {
-        // Display even numbers after 1 second
-        setTimeout(() => {
-            updateOutput(filterOddNumbers(numbers).join(', '));
-        }, 1000);
-        return numbers;
-    })
-    .then(numbers => {
-        // Multiply even numbers by 2 after 2 seconds
-        setTimeout(() => {
-            updateOutput(multiplyEvenNumbers(numbers).join(', '));
-        }, 2000);
-    });
+    const inputArray = [1, 2, 3, 4];
+    manipulateData(inputArray);
